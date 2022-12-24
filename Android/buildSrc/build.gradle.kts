d79e9d9e45701eff97fac4aab45dacf2aba519c6
ParkJong-Hun <thesn515@gmail.com>
2022-12-24 15:56:53 +0900

    feat:create buildSrc

---
+++ Android/buildSrc/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    // Kotlin Domain-Specific Language
+    // ref https://developer.android.com/guide/navigation/navigation-kotlin-dsl
+    `kotlin-dsl`
+}
+
+repositories {
+    // Maven Central ⊂ JCenter
+    // https://blog.loginradius.com/engineering/how-to-use-jcenter-dependencies-in-android-studio/
+    jcenter()
+}
---
+++ Android/buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,32 @@
+object Versions {
+    const val COMPILE_SDK = 33
+    const val MIN_SDK = 28
+    const val TARGET_SDK = 33
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+    const val ANDROID = "7.1.2"
+    const val KOTLIN = "1.5.21"
+
+    object Default {
+        const val CORE = "1.7.0"
+        const val JUNIT = "4.13.2"
+        const val LIFE_CYCLE = "2.3.1"
+        const val ACTIVITY_COMPOSE = "1.3.1"
+        const val EXT_JUNIT = "1.1.3"
+        const val ESPRESSO_CORE = "3.4.0"
+    }
+
+    object Compose {
+        const val COMPOSE = "1.0.1"
+        const val NAVIGATION = "2.5.3"
+    }
+
+    object DI {
+        const val HILT = "2.38.1"
+    }
+
+    object Retrofit {
+        const val RETROFIT = "2.9.0"
+    }
+}
