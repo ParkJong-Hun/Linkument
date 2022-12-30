4f469a15792dc5f7e8cfdd52efacc490cb41eabb
ParkJong-Hun <thesn515@gmail.com>
2022-12-30 12:35:29 +0900

    create a API project

---
+++ API/src/main/kotlin/com/pjh/linkumentapi/LinkumentApiApplication.kt
@@ -0,0 +1,11 @@
+package com.pjh.linkumentapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinkumentApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinkumentApiApplication>(*args)
+}
