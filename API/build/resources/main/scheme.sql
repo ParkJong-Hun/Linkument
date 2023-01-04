9c26e825827c29854e769495558c26097902c6d6
ParkJong-Hun <thesn515@gmail.com>
2023-01-04 12:30:28 +0900

    fix:add root .gitignore

--- API/build/resources/main/scheme.sql
+++
@@ -1,8 +0,0 @@
-create TABLE if not exists `user`(
-    `id` BIGINT unsigned not null auto_increment,
-    `nickname` VARCHAR(20) not null,
-    `create_time` DATETIME not null default current_timestamp,
-    `update_time` DATETIME not null default current_timestamp on update current_timestamp,
-    PRIMARY KEY(`id`),
-    UNIQUE KEY `nickname`(`nickname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
