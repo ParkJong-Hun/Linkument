e7e5088b8af5cd94b20a1329906424934ffe9dd1
ParkJong-Hun <thesn515@gmail.com>
2023-01-07 19:02:29 +0900

    feat:write dummy code

--- Android/app/src/main/java/com/pjh/linkument_android_app/MainActivity.kt
+++ Android/app/src/main/java/com/pjh/linkument_android_app/MainActivity.kt
@@ -3,14 +3,16 @@ package com.pjh.linkument_android_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pjh.linkument_android_app.ui.theme.AppTheme
+import com.pjh.linkument_android_app.ui.theme.md_theme_light_secondary
+import com.pjh.linkument_android_app.ui.theme.md_theme_light_tertiary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +21,39 @@ class MainActivity : ComponentActivity() {
             AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(modifier = Modifier
+                            .width(100.dp)
+                            .height(60.dp),
+                            onClick = { /*TODO*/ }) {
+                            Greeting("Android")
+                        }
+                        OutlinedButton(modifier = Modifier
+                            .width(100.dp)
+                            .height(60.dp),
+                            onClick = { /*TODO*/ }) {
+                            Greeting("Android")
+                        }
+                        Button(modifier = Modifier
+                            .width(100.dp)
+                            .height(60.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = md_theme_light_secondary),
+                            onClick = { /*TODO*/ }) {
+                            Greeting("Android")
+                        }
+                        Button(modifier = Modifier
+                            .width(100.dp)
+                            .height(60.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = md_theme_light_tertiary),
+                            onClick = { /*TODO*/ }) {
+                            Greeting("Android")
+                        }
+                    }
                 }
             }
         }
