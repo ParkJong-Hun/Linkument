53537e784ba4f0794f16a3bdcc60bf7d3de96d09
ParkJong-Hun <thesn515@gmail.com>
2023-02-11 17:12:49 +0900

    feat: double button

--- Android/app/src/main/java/com/pjh/linkument_android_app/ui/template/button/DoubleButton.kt
+++ Android/app/src/main/java/com/pjh/linkument_android_app/ui/template/button/DoubleButton.kt
@@ -1,21 +1,74 @@
 package com.pjh.linkument_android_app.ui.template.button
 
-import androidx.compose.foundation.layout.Row
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pjh.linkument_android_app.ui.theme.AppTheme
+import com.pjh.linkument_android_app.ui.theme.ButtonColorType
 
 @Composable
 fun DoubleButton(
-    leftButtonText: () -> Unit,
-    rightButtonText: () -> Unit
+    modifier: Modifier = Modifier,
+    leftButtonText: String,
+    onClickLeftButton: () -> Unit,
+    leftButtonEnabled: Boolean = true,
+    leftButtonColorType: ButtonColorType = ButtonColorType.Primary,
+    rightButtonText: String,
+    onClickRightButton: () -> Unit,
+    rightButtonEnabled: Boolean = true,
+    rightButtonColorType: ButtonColorType = ButtonColorType.Secondary,
 ) {
-    Row() {
-        // TODO
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        BasicButton(
+            modifier = Modifier.weight(1f),
+            onClick = { onClickLeftButton },
+            colorType = leftButtonColorType,
+            text = leftButtonText,
+            enabled = leftButtonEnabled
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        BasicButton(
+            modifier = Modifier.weight(1f),
+            onClick = { onClickRightButton },
+            colorType = rightButtonColorType,
+            text = rightButtonText,
+            enabled = rightButtonEnabled
+        )
     }
 }
 
-@Preview
+@Preview(group = "LightMode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun DoubleButtonPreview() {
+private fun DoubleButtonLightPreview() {
+    AppTheme {
+        DoubleButton(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+            leftButtonText = "Agree",
+            onClickLeftButton = {},
+            rightButtonText = "Not Agree",
+            onClickRightButton = {})
+    }
+}
 
+@Preview(group = "DarkMode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun DoubleButtonDarkPreview() {
+    AppTheme {
+        DoubleButton(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+            leftButtonText = "Agree",
+            onClickLeftButton = {},
+            rightButtonText = "Not Agree",
+            onClickRightButton = {})
+    }
 }
